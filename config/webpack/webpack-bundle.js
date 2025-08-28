cd59a59fa7bc5330af064af2be2e0a4921e1545b
sergiolecegui <sergio.lecegui@alma-medical.com>
2025-08-28 16:23:38 +0200

    cofig-dev: v.1.1

--- config/webpack/webpack-bundle.js
+++ config/webpack/webpack-bundle.js
@@ -8,7 +8,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 // The big difference between this and the dynamic-import version is that
 // the dynamic import version does not bundle the WASM modules into the WebWorker file
 module.exports = {
-  mode: 'development',
+  mode: 'development', // to develop
   context,
   stats: {
     children: true
